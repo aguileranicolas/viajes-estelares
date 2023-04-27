ab0f061ffd0ed1b0a2b5a7c5b0ae25d382caa560
Nicolás Aguilera <aguileranicolas755@gmail.com>
2023-04-27 16:01:13 -0300

    Cambios about

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -4,19 +4,21 @@ import flag from '../assets/images/background/Flag_of_Argentina.svg'
 const About = () => {
 	return (
 		<>
-			<div className='about-container'>
-				<div className='imagen-container'>
-					<img src={profile} alt='Imagen de Hugo Nicolas Aguilera' />
-				</div>
-				<div className='info-container'>
-					<h2>Nombre: Hugo Nicolas Aguilera</h2>
-					<p>Edad: 20</p>
-					<p>Profesión: Fullstack Developer at Henry Bootcamp</p>
-					<p>Cohorte: FT36a</p>
-					<p>
-						Location: Posadas, Misiones{' '}
-						<img src={flag} alt='Banderita de Argentina' />
-					</p>
+			<div className='fondoAbout'>
+				<div className='about-container'>
+					<div className='imagen-container'>
+						<img src={profile} alt='Imagen de Hugo Nicolas Aguilera' />
+					</div>
+					<div className='info-container'>
+						<h2>Nombre: Hugo Nicolas Aguilera</h2>
+						<p>Edad: 20</p>
+						<p>Profesión: Fullstack Developer at Henry Bootcamp</p>
+						<p>Cohorte: FT36a</p>
+						<p>
+							Location: Posadas, Misiones{' '}
+							<img src={flag} alt='Banderita de Argentina' />
+						</p>
+					</div>
 				</div>
 			</div>
 		</>
